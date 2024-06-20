2436a6b1b24e4ebf7afffd9487b03a4cf4ce66ea
Sebastian Zambrano <sebastian9zambrano10@gmail.com>
2024-06-19 22:27:08 -0500

    primer compromiso

---
+++ index.js
@@ -0,0 +1,22 @@
+// 1. Concatenar elementos posteriores al Array
+'Use Strict';
+
+let nums = [0,2,4,8];
+
+for (let i of nums)
+    {
+    for (let j of nums)
+        {
+        alert ( i+ ""+j);
+    }
+}; 
+
+functionimprimirpatron (array); {
+    for (let i = 0 ; i < array.lenght; i++){
+        for(let j = 0; j < array.lenght; j++){
+            let resultado = num[i] + num [j];
+            alert (resultado) 
+        }
+    }
+}
+
